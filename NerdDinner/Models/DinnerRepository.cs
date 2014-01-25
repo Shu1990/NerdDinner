b92639dccdb1242585737324f18bef7119ebf71c
shu1990 <SU@paraport.com>
2014-01-24 16:33:24 -0800

    First Commit

---
+++ NerdDinner/Models/DinnerRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NerdDinner.Models;
+
+namespace NerdDinner.Models
+{
+    public class DinnerRepository
+    {
+        private NerdDinnerDataContext db = new NerdDinnerDataContext();
+
+        //
+        // Query Methods
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return db.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinner GetDinner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        //
+        // Insert/Delete Methods
+        public void Add(Dinner dinner)
+        {
+            db.Dinners.InsertOnSubmit(dinner);
+        }
+
+        public void Delete(Dinner dinner)
+        {
+            db.RSVPs.DeleteAllOnSubmit(dinner.RSVPs);
+            db.Dinners.DeleteOnSubmit(dinner);
+        }
+
+        //
+        // Persistence
+        public void Save()
+        {
+            db.SubmitChanges();
+        }
+    }
+}
+
+namespace NerdDinner.Controllers
+{
+    public class DinnersController : Controller
+    {
+        DinnerRepository dinnerRepository = new DinnerRepository();
+        //
+        // GET: /Dinners/
+        public ActionResult Index()
+        {
+            var dinners = dinnerRepository.FindUpcomingDinners().ToList();
+
+            return View("Index", dinners);
+        }
+        //
+        // GET: /Dinners/Details/2
+        public ActionResult Details(int id)
+        {
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+            if (dinner == null)
+                return View("NotFound");
+            else
+                return View("Details", dinner);
+        }
+    }
+}
